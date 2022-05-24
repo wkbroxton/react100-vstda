35122c66c6ceff38c05474fd1e3b38b992dc18be
William Kevin Broxton <90276407+wkbroxton@users.noreply.github.com>
2022-05-24 16:30:57 -0700

    list group implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,74 @@
 import React, { Component } from "react";
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodo: "",
+      priority: 0,
+      editEnabled: true,
+      showTodoList: false,
+    };
+    this.onChange = this.onChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  onChange(e) {
+    const newTodo = e.target.newTodo;
+    this.setState({ [newTodo]: value });
+  }
+
+  handleClick(e) {
+    const newTodo = this.state.newTodo;
+    const priority = this.state.priority;
+    const editEnabled = this.state.editEnabled;
+
+    this.setState({
+      newTodo: newTodo,
+      priority: priority,
+      editEnabled: editEnabled,
+      showTodoList: true,
+    });
+  }
+
   render() {
     return (
       <div className="container">
-        <h1 id="heading" className="title">
-          Very Simple To Do App
-        </h1>
-        <h4>Track all of the things</h4>
-        <hr />
+        <div className="title">
+          <h1 id="heading">Very Simple To Do App</h1>
+          <h4>Track all of the things</h4>
+          <hr />
+        </div>
         <div className="container" id="addToDo">
-          <Card>
-            <Card.Header as="h5">Featured</Card.Header>
-            <Card.Body>
-              <Card.Title>Special title treatment</Card.Title>
-              <Card.Text>
-                With supporting text below as a natural lead-in to additional
-                content.
-              </Card.Text>
-              <textarea
-                name=""
-                id=""
-                cols="30"
-                rows="10"
-                className="create-todo-text"
-              ></textarea>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
+          <h4>I want to...</h4>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            className="create-todo-text"
+          ></textarea>
+          <h4>How much of a priority is this?</h4>
+          <select name="" id="">
+            <option values="0" defaultValue={"default"} disabled>
+              Select a Priority
+            </option>
+            <option values="1">Low</option>
+            <option values="3">Medium</option>
+            <option values="3">High</option>
+          </select>
+          <button>Save</button>
+        </div>
+        <div className="container" id="todo-list">
+          <div>
+            <ListGroup>
+              <ListGroupItem bsStyle="success">Success</ListGroupItem>
+              <ListGroupItem bsStyle="warning">Warning</ListGroupItem>
+              <ListGroupItem bsStyle="danger">Danger</ListGroupItem>
+            </ListGroup>
+          </div>
         </div>
       </div>
     );
   }
 }
-
-export default App;
