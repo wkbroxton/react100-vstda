768100772f6ac04da063b43c27465766b1642dd8
William Kevin Broxton <90276407+wkbroxton@users.noreply.github.com>
2022-05-24 13:06:34 -0700

    to do list beginnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-
   render() {
     return (
-      <div className='container'>
-
+      <div className="container">
+        <h1 id="heading" className="title">
+          Very Simple To Do App
+        </h1>
+        <h4>Track all of the things</h4>
+        <hr />
+        <div className="container" id="addToDo">
+          <Card>
+            <Card.Header as="h5">Featured</Card.Header>
+            <Card.Body>
+              <Card.Title>Special title treatment</Card.Title>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </Card.Text>
+              <textarea
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+                className="create-todo-text"
+              ></textarea>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
     );
   }
