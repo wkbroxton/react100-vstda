1897df2357ea08d503058568ea8fe43eb38e7450
William Kevin Broxton <90276407+wkbroxton@users.noreply.github.com>
2022-05-27 16:51:40 -0700

    static Todo renders onClick

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default class App extends Component {
       color: "",
       editEnabled: false,
       allTodos: [],
-      viewTodos: false,
+      showTodos: false,
     };
     this.onChange = this.onChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -37,10 +37,15 @@ export default class App extends Component {
       priority: priority,
       color: color,
       editEnabled: editEnabled,
-      viewTodos: true,
     };
     let newArr = this.state.allTodos.concat(todo);
-    this.setState({ allTodos: newArr, newTodo: "", priority: 0, color: "" });
+    this.setState({
+      allTodos: newArr,
+      newTodo: "",
+      priority: 0,
+      color: "",
+      showTodos: true,
+    });
   }
 
   render() {
@@ -56,7 +61,7 @@ export default class App extends Component {
 
         <div className="row">
           <div className="container col-4" id="addToDo">
-            <h4>I want to...</h4>
+            <h4 id="want">I want to...</h4>
             <textarea
               cols="30"
               rows="10"
@@ -83,175 +88,179 @@ export default class App extends Component {
           </div>
 
           <div className="container col-8" id="todo-list">
-            <div id="welcome-phrase-div">
-              <h4>ToDo Items</h4>
-              <h5 id="welcome-phrase">Welcome to Very Simple Todo App!</h5>
-              <h5>Get started now by adding a new todo on the left.</h5>
-            </div>
+            {this.state.showTodos ? (
+              <div>
+                <h4>ToDo Items</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    First checkbox
+                  </li>
+                  <li className="list-group-item">
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    Second checkbox
+                  </li>
+                  <li className="list-group-item">
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    Third checkbox
+                  </li>
+                  <li className="list-group-item">
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    Fourth checkbox
+                  </li>
+                  <li className="list-group-item">
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    Fifth checkbox
+                  </li>
+                </ul>
 
-            <hr />
+                <hr />
+                <div className="list-group ">
+                  <a
+                    href="#"
+                    className="list-group-item list-group-item-action view"
+                  >
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    A simple default list group item
+                  </a>
 
-            <ul className="list-group">
-              <li className="list-group-item">
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                First checkbox
-              </li>
-              <li className="list-group-item">
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                Second checkbox
-              </li>
-              <li className="list-group-item">
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                Third checkbox
-              </li>
-              <li className="list-group-item">
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                Fourth checkbox
-              </li>
-              <li className="list-group-item">
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                Fifth checkbox
-              </li>
-            </ul>
-
-            <hr />
-            <div className="list-group ">
-              <a
-                href="#"
-                className="list-group-item list-group-item-action view"
-              >
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                A simple default list group item
-              </a>
-
-              <a
-                href="#"
-                className="list-group-item list-group-item-action list-group-item-primary"
-              >
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                A simple primary list group item
-              </a>
-              <a
-                href="#"
-                className="list-group-item list-group-item-action list-group-item-secondary"
-              >
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                A simple secondary list group item
-              </a>
-              <a
-                href="#"
-                className="list-group-item list-group-item-action list-group-item-success"
-              >
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                A simple success list group item
-              </a>
-              <a
-                href="#"
-                className="list-group-item list-group-item-action list-group-item-danger"
-              >
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                A simple danger list group item
-              </a>
-              <a
-                href="#"
-                className="list-group-item list-group-item-action list-group-item-warning"
-              >
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                A simple warning list group item
-              </a>
-              <a
-                href="#"
-                className="list-group-item list-group-item-action list-group-item-info"
-              >
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                A simple info list group item
-              </a>
-              <a
-                href="#"
-                className="list-group-item list-group-item-action list-group-item-light"
-              >
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                A simple light list group item
-              </a>
-              <a
-                href="#"
-                className="list-group-item list-group-item-action list-group-item-dark"
-              >
-                <input
-                  className="form-check-input me-1"
-                  type="checkbox"
-                  value=""
-                  aria-label="..."
-                />
-                A simple dark list group item
-              </a>
-            </div>
-            <p id="todo-list">{this.state.allTodos.newTodo}</p>
+                  <a
+                    href="#"
+                    className="list-group-item list-group-item-action list-group-item-primary"
+                  >
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    A simple primary list group item
+                  </a>
+                  <a
+                    href="#"
+                    className="list-group-item list-group-item-action list-group-item-secondary"
+                  >
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    A simple secondary list group item
+                  </a>
+                  <a
+                    href="#"
+                    className="list-group-item list-group-item-action list-group-item-success"
+                  >
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    A simple success list group item
+                  </a>
+                  <a
+                    href="#"
+                    className="list-group-item list-group-item-action list-group-item-danger"
+                  >
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    A simple danger list group item
+                  </a>
+                  <a
+                    href="#"
+                    className="list-group-item list-group-item-action list-group-item-warning"
+                  >
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    A simple warning list group item
+                  </a>
+                  <a
+                    href="#"
+                    className="list-group-item list-group-item-action list-group-item-info"
+                  >
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    A simple info list group item
+                  </a>
+                  <a
+                    href="#"
+                    className="list-group-item list-group-item-action list-group-item-light"
+                  >
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    A simple light list group item
+                  </a>
+                  <a
+                    href="#"
+                    className="list-group-item list-group-item-action list-group-item-dark"
+                  >
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    A simple dark list group item
+                  </a>
+                </div>
+                <p id="todo-list">{this.state.allTodos.newTodo}</p>
+              </div>
+            ) : (
+              <div id="welcome-phrase-div">
+                <h4>ToDo Items</h4>
+                <h5 id="welcome-phrase">Welcome to Very Simple Todo App!</h5>
+                <h5>Get started now by adding a new todo on the left.</h5>
+                <hr />
+              </div>
+            )}
           </div>
         </div>
       </div>
